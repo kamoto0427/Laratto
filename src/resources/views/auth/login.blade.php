8bbb86f5916522f009f262dba36a896a9580b12d
kamoto <takahiro.08039@gmail.com>
2022-04-04 23:32:28 +0900

    新規登録・ログインエラーメッセージ日本語対応

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-auth-validation-errors class="mb-4 text-center" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
